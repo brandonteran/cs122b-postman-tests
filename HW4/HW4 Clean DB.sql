276f9f4088f9275ef7881c940308f9210239ab8f
Alexander Iskander <Alex.Iskander@Gmail.com>
2019-05-01 16:19:00 -0700

    Added Order Tests

--- HW4/HW4 Clean DB.sql
+++ HW4/HW4 Clean DB.sql
@@ -3,3 +3,5 @@ DELETE FROM creditcards WHERE id = '9876543210987654321';
 DELETE FROM customers WHERE email = 'newCustomer@uci.edu';
 
 DELETE FROM carts WHERE email = 'adah.glen.232@facebook.com' AND movieId = 'tt0308514';
+
+DELETE FROM sales WHERE email = 'adah.glen.232@facebook.com';
