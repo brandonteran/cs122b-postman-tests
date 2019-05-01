ec2021c89ea863b66a2731423db2b50f2af1104d
Alexander Iskander <Alex.Iskander@Gmail.com>
2019-05-01 15:39:51 -0700

    Added Shopping Cart DB Tests

--- HW4/HW4 Clean DB.sql
+++ HW4/HW4 Clean DB.sql
@@ -1,5 +1,5 @@
 DELETE FROM creditcards WHERE id = '9876543210987654321';
 
+DELETE FROM customers WHERE email = 'newCustomer@uci.edu';
 
-
-DELETE FROM customers WHERe email = 'newCustomer@uci.edu';
+DELETE FROM carts WHERE email = 'adah.glen.232@facebook.com' AND movieId = 'tt0308514';
