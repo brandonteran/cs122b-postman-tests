aa0ad365c49dc02987d62fc46a88204f3eba57c3
Alexander Iskander <Alex.Iskander@Gmail.com>
2019-05-02 09:59:28 -0700

    Changed Tests to Not Effect DB
    Changed Tests so that they do not effect Orginal DB Entrys on failute

--- HW4/HW4 Clean DB.sql
+++ HW4/HW4 Clean DB.sql
@@ -1,7 +1,23 @@
-DELETE FROM creditcards WHERE id = '9876543210987654321';
+DELETE 
+FROM creditcards
+WHERE id = '9876543210987654321'
+   OR id = 'A999888877776666123'
+   OR id = '999888877776666123'
+   OR id = '9999888877776666123';
 
-DELETE FROM customers WHERE email = 'newCustomer@uci.edu';
+DELETE 
+FROM customers
+WHERE email = 'newCustomer@uci.edu'
+   OR email = '@uci.edu'
+   OR email = 'peteranteater@uci.edu'
+   OR email = 'notExistEmail@notreal.com'
+   OR email = 'abcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyz@uci.edu';
 
-DELETE FROM carts WHERE email = 'adah.glen.232@facebook.com' AND movieId = 'tt0308514';
+DELETE 
+FROM carts 
+WHERE email = 'adah.glen.232@facebook.com' 
+  AND movieId = 'tt0308514';
 
-DELETE FROM sales WHERE email = 'adah.glen.232@facebook.com';
+DELETE 
+FROM sales 
+WHERE email = 'adah.glen.232@facebook.com';
